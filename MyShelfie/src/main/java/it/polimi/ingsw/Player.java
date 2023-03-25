2cd52f0a7a8834b84aacbcbb690a0c01ed3a1220
Simone <simone2.oggioni@mail.polimi.it>
2023-03-25 18:33:31 +0100

    try commit

--- MyShelfie/src/main/java/it/polimi/ingsw/Player.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Player.java
@@ -18,6 +18,61 @@ public class Player{
 
     public Player(String nickname){
 
+        this.nickname=nickname;
+        this.score=score;
+        this.firstPlayer=firstPlayer;
+        this.firstCommonGoalReached=firstCommonGoalReached;
+        this.secondCommonGoalReached=secondCommonGoalReached;
+        this.hasEndGameToken=hasEndGameToken;
+
+    }
+
+    public boolean isFirstPlayer() {
+        return firstPlayer;
+    }
+
+    public void setFirstPlayer(boolean firstPlayer) {
+        this.firstPlayer = firstPlayer;
+    }
+
+    public boolean isFirstCommonGoalReached() {
+        return firstCommonGoalReached;
+    }
+
+    public void setFirstCommonGoalReached(boolean firstCommonGoalReached) {
+        this.firstCommonGoalReached = firstCommonGoalReached;
+    }
+
+    public boolean isSecondCommonGoalReached() {
+        return secondCommonGoalReached;
+    }
+
+    public void setSecondCommonGoalReached(boolean secondCommonGoalReached) {
+        this.secondCommonGoalReached = secondCommonGoalReached;
+    }
+
+    public boolean isHasEndGameToken() {
+        return hasEndGameToken;
+    }
+
+    public void setHasEndGameToken(boolean hasEndGameToken) {
+        this.hasEndGameToken = hasEndGameToken;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
     public List<Position> chooseCards(){
