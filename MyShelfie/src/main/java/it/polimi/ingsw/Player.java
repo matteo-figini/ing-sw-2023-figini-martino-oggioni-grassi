2d30b6dfc6fbd626f4269a76041ff50043179f7d
fedeg <federico4.grassi@mail.polimi.it>
2023-03-25 15:26:57 +0100

    Class Player layout

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Player.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw;
+
+
+public class Player{
+
+    private String nickname;
+
+    private int score = 0;
+
+    private boolean firstPlayer = false;
+
+    private boolean firstCommonGoalReached = false;
+
+    private boolean secondCommonGoalReached = false;
+
+    private boolean hasEndGameToken = false;
+
+
+    public Player(String nickname){
+
+    }
+
+    public List<Position> chooseCards(){
+
+    }
+
+    public List<ObjectCard> rearrangeCards(List<ObjectCard> unordered){
+
+    }
+}
